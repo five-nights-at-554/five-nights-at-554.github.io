3aa2156c06bab08367b5ed69e47d6e93032b9019
pmif <narlor200@gmail.com>
2024-11-11 18:40:55 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,81 @@
+contextMenuTF = true
+
+function createSound(path) {
+	let audio = new Audio()
+	audio.src = path
+	return(audio)
+}
+
+const AUDIO = {
+	calm: createSound('/js/sounds/calm.mp3'),
+	mainMenu: createSound('/js/sounds/mainMenu.mp3')
+}
+
+function muz_first() {
+	AUDIO.mainMenu.play()
+	AUDIO.mainMenu.loop = true
+	AUDIO.mainMenu.autoplay = true
+}
+
+function f11Message() {
+	let message = document.getElementById('press-f11')
+	setTimeout(function() {
+		message.style.opacity = 0
+	}, 1000)
+	setTimeout(function() {
+		message.style.display = 'none'
+	}, 3000)
+}
+
+document.addEventListener('keydown', function(event) {
+	if (event.key === 'Enter') {
+		document.getElementById('disc_accept').click()
+	}
+})
+function discClose() {
+	let disc_pass_input = document.getElementById('disc-pass')
+	disc_pass = disc_pass_input.value
+	if (disc_pass === '1234') {
+		disc_pass_input.style.border = 'solid 0.1rem green'
+		f11Message()
+		muz_first()
+		document.getElementById('disclaimer').style.display = 'none'
+		contextMenuTF = true
+	}
+	else {
+		disc_pass_input.style.outline = 'solid 0.1rem #bd0707'
+		disc_pass_input.value = ''
+	}
+}
+
+const contextMenu = document.getElementById('context-menu')
+
+    window.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+
+        if (contextMenuTF) {
+			const { clientX: mouseX, clientY: mouseY } = e
+
+        	contextMenu.style.left = mouseX + 'px'
+        	contextMenu.style.top = mouseY + 'px'
+
+        	const { innerHeight, innerWidth } = window
+        	const menuHeight = contextMenu.offsetHeight
+        	const menuWidth = contextMenu.offsetWidth
+
+        	if (mouseY + menuHeight > innerHeight) {
+        	    contextMenu.style.top = (mouseY - menuHeight) + 'px'
+        	}
+
+        	if (mouseX + menuWidth > innerWidth) {
+        	    contextMenu.style.left = (mouseX - menuWidth) + 'px'
+        	}
+
+        	contextMenu.style.display = 'block'
+			}
+    })
+
+    window.addEventListener('click', () => {
+        contextMenu.style.display = 'none'
+    })
+
