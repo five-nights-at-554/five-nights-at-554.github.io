a811efb61ab39473e3b95faf268e37d27a1ce322
pmif <narlor200@gmail.com>
2024-11-14 23:03:26 +0300

    asd

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 var audTF = true
 
-const BG = {
-	
-}
+const BG = [
+
+]
 
 document.addEventListener('visibilitychange', function() {
 	if (audTF) {
@@ -77,33 +77,99 @@ window.addEventListener('click', () => {
     contextMenu.style.display = 'none'
 })
 
-let lef = 2
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowLeft') {
-		if (!settingsOn) {
-			lef = lef - 2
-			document.getElementById('gg').src = 'images/fil-left.png'
-			document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
-		}
-        console.log('Нажата стрелка влево')
-    } else if (event.key === 'ArrowRight') {
-		if (!settingsOn) {
-			lef = lef + 2
-			document.getElementById('gg').src = 'images/fil-right.png'
-			document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+// let lef = 2
+// document.addEventListener('keydown', (event) => {
+	//     if (event.key === 'ArrowLeft') {
+		// 		if (!settingsOn) {
+			// 			lef = lef - 1.7
+			// 			document.getElementById('gg').src = 'images/fil-left.png'
+			// 			document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+			// 		}
+			//         console.log('Нажата стрелка влево')
+			//     } else if (event.key === 'ArrowRight') {
+				// 		if (!settingsOn) {
+					// 			lef = lef + 1.7
+					// 			document.getElementById('gg').src = 'images/fil-right.png'
+					// 			document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+					
+					// 		}
+					// 		console.log('Нажата стрелка')
+//     }
 
-		}
-		console.log('Нажата стрелка')
+// 	if (lef <= 0) {
+// 		lef = 155
+// 		document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+// 		--bg_number
+// 		document.getElementById('game-bg').src = `images/${bg_number}-game-bg.jpg`
+// 	}
+// 	else if (lef >= 156.5) {
+// 		lef = 2
+// 		document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+// 		++bg_number
+// 		document.getElementById('game-bg').src = `images/${bg_number}-game-bg.jpg`
+// 	}
+// 	 else if (lef > 0 && lef < 156.5) {
+	// 		document.getElementById('gg').style.display = 'inline-block'
+	// 	}
+	// })
+	
+	
+	
+	let isArrowLeftPressed = false // Флаг для отслеживания нажатия стрелки влево
+	let isArrowRightPressed = false // Флаг для отслеживания нажатия стрелки вправо
+	
+	const gg = document.getElementById('gg') // Получаем элемент
+	
+	// Функция для перемещения объекта
+let lef = 3
+let bg_number = 2
+
+function movegg() {
+    if (isArrowLeftPressed) {
+ 
+        lef = lef - 1.5
+        gg.style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+    }
+    if (isArrowRightPressed) {
+
+        lef = lef + 1.5
+        gg.style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
     }
 
-	if (lef <= 0 || lef >= 156.5) {
-		document.getElementById('gg').style.display = 'none'
-	} else if (lef > 0 && lef < 156.5) {
-		document.getElementById('gg').style.display = 'inline-block'
+	if (lef <= 2) {
+		lef = 148.5
+		document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+		--bg_number
+		document.getElementById('game-bg').src = `images/${bg_number}-game-bg.jpg`
 	}
-})
+	else if (lef >= 150) {
+		lef = 2
+		document.getElementById('gg').style.left = `calc((100vw - 100vh * 16 / 9) / 2 + 2vh + ${lef}vh)`
+		++bg_number
+		document.getElementById('game-bg').src = `images/${bg_number}-game-bg.jpg`
+	}
 	
+    requestAnimationFrame(movegg)
+}
 
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft' && !isArrowLeftPressed) {
+		document.getElementById('gg').src = 'images/fil-left.png'
+        isArrowLeftPressed = true
+    } else if (event.key === 'ArrowRight' && !isArrowRightPressed) {
+		document.getElementById('gg').src = 'images/fil-right.png'
+        isArrowRightPressed = true
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'ArrowLeft') {
+        isArrowLeftPressed = false
+    } else if (event.key === 'ArrowRight') {
+        isArrowRightPressed = false
+    }
+})
+requestAnimationFrame(movegg)
 
 
 
